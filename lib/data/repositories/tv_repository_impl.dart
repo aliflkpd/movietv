0849564a28b3ed67b00f9429acdc695c4bca3751
Aliflkpd <andialifnurman@gmail.com>
2022-12-11 08:07:03 +0800

    update ssl

--- lib/data/repositories/tv_repository_impl.dart
+++ lib/data/repositories/tv_repository_impl.dart
@@ -28,6 +28,10 @@ class TvRepositoryImpl implements TvRepository {
       return Left(ServerFailure(''));
     } on SocketException {
       return Left(ConnectionFailure('Failed to connect to the network'));
+    } on TlsException {
+      return const Left(CommonFailure('Certificted not valid!'));
+    } catch (e) {
+      return Left(CommonFailure(e.toString()));
     }
   }
 
